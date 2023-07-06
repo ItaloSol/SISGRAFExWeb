fe48e9ae62067ac93f383a0bdca3d162ab4eae1a
ItaloSol <italo.sol100@gamil.com>
2023-07-06 10:45:35 -0300

    ajuste de arquivos

--- html/navbar.php
+++ html/navbar.php
@@ -72,8 +72,6 @@ $refresh = 0;
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-<!-- Arquivos CSS do Bootstrap -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
   <!-- Icons. Uncomment required icon fonts -->
   <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
