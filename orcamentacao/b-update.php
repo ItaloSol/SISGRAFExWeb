5e8f8c414237b8b77c3bc9339e30ed21b6069565
ItaloSol <italo.sol100@gamil.com>
2023-07-17 14:17:07 -0300

    restatura orçamento para em avaliação

--- orcamentacao/b-update.php
+++ orcamentacao/b-update.php
@@ -245,6 +245,32 @@ if (isset($_GET['acao'])) {
     $Atividade_Supervisao->execute();
     header("Location: tl-orcamento.php?cod=$cod");
   }
+  if ($_GET['acao'] == '9') {
+    $query_aceitalas = $conexao->prepare("UPDATE tabela_orcamentos SET status = '1'  WHERE cod = '$cod' ");
+    $query_aceitalas->execute();
+    $_SESSION['msg'] = ' <div id="alerta"
+            role="bs-toast"
+            class=" bs-toast toast toast-placement-ex m-3 fade bg-success top-0 end-0 hide show "
+            role="alert"
+            aria-live="assertive"
+            aria-atomic="true">
+            <div class="toast-header">
+              <i class="bx bx-bell me-2"></i>
+              <div class="me-auto fw-semibold">Aviso!</div>
+              <small>
+                
+                </small>
+              <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            
+            <div class="toast-body">
+                 Orçamento Voltou para em avaliação!    
+            </div>
+          </div>';
+    $Atividade_Supervisao = $conexao->prepare("INSERT INTO supervisao_atividade (alteracao_atividade , atendente_supervisao, data_supervisao) VALUES ('Orçamento $cod Voltou para em Avaliação ' , '$cod_user' , '$dataHora')");
+    $Atividade_Supervisao->execute();
+    header("Location: tl-orcamento.php?cod=$cod");
+  }
   if ($_GET['acao'] == '11') {
     $query_aceitalas = $conexao->prepare("UPDATE tabela_orcamentos SET status = '11'  WHERE cod = '$cod' ");
     $query_aceitalas->execute();
